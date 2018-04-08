e8522084896f96ac554c645a045c8795c4b05aff
ErikJP <erik.pronk@colorado.edu>
2018-04-08 15:06:42 -0600

    Adding listing table

--- Databases/images_table.sql
+++ Databases/images_table.sql
@@ -1,8 +1,8 @@
 create table if not exists images(
 	image_id int(11) not null auto_increment,
-	user_id int(11) not null,
+	list_id int(11) not null,
 	image blob not null,
 	description varchar(128) not null,
 	primary key (image_id),
-	foreign key (user_id) references user_info(id)
+	foreign key (list_id) references listing(listing_id)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
---
+++ Databases/listing_table.sql
@@ -0,0 +1,11 @@
+create table if not exists listing(
+	listing_id int(11) not null auto_increment,
+	user_id int(11) not null,
+	breed varchar(40) not null,
+	age = int(11) not null,
+	gender = varchar(5) not null,
+	description varchar(128) not null,
+	day_listed datetime default current_timestamp,
+	primary key (listing_id),
+	foreign key (user_id) references user_info(id)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
